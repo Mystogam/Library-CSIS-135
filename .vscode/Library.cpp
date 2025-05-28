ed419d14d2f8e866782de7c379ae13170625c71b
Christopher Alexan <chrisalexan123@yahoo.com>
2025-05-28 15:37:27 -0700

    Created cout statement that welcomes user and informs them of the information below.

--- .vscode/Library.cpp
+++ .vscode/Library.cpp
@@ -8,7 +8,8 @@ using namespace std;
 
 long long LibraryMenu()
 {
-
+    cout << "Welcome to the GCC computer science library!!\n Below you will find a menu that will ask you to decide what to do next.\n" << endl;
+    
     cout << "If checking for book info in library please press 1 to go to book search menu and prompt.\n" << endl;
 
     cout << "If information is already known and you wish to borrow a book from the library please press 2 to go to the next menu and prompt.\n" << endl;
