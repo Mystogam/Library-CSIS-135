0746ea4323505bab679487e5feac4a0916de17b1
Christopher Alexan <chrisalexan123@yahoo.com>
2025-05-30 19:32:59 -0700

    added the prompt for all the different cout statements in 3 of the void functions.

--- .vscode/Library.cpp
+++ .vscode/Library.cpp
@@ -72,22 +72,27 @@ void AddBook(std::string title, std::string author, int publicationYear, int cop
 {
     cout << "Thank you for inputting the third selection and enter the add book to archive section of the library.\n" << endl;
     cout << "Please enter the name or title of the book you would like to add to the library: " << endl;
+    cin >> title;
     cout << "Please enter the name of the author of the book that is being added to the library: " << endl;
+    cin >> author;
     cout << "Please enter the year in which the book being added was published: " << endl;
+    cin >> publicationYear;
     cout << "Please also declare that amount of copies that you would be adding of said book to the Library: " << endl;
-    
+    cin >> copiesAvailable;
 }
 
 void RemoveBook(std::string title)
 {
     cout << "Thank you for inputting the fourth selection and entering the remove book section of this library.\n" << endl;
     cout << "Please enter the name or title of book you would like to remove from the Library: " << endl;
+    cin >> title;
 }
 
 void BorrowBook(std::string title)
 {
     cout << "Thank you for inputting the second selection and entering the borrowing and free renting section of this library\n" << endl;
     cout << "Please enter the name or title of the book that you would be borrowing from the Library: " << endl;
+    cin >> title;
 }
 void DisplayBook()
 {
