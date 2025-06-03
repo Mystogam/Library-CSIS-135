8d90d6fc23924e74769ea29390f0d78804a34a2b
Christopher Alexan <chrisalexan123@yahoo.com>
2025-06-02 19:02:35 -0700

    cleaning up void adbook function. Moving the prompts for add book to the if else statement chain selection 3

--- .vscode/Library.cpp
+++ .vscode/Library.cpp
@@ -11,8 +11,11 @@ int main()
 {
     Library myLibrary;//Pulls the Library Class from the header file and allows for the .
 
-
+    int publicationYear;
+    int copiesAvailable;
     int selection;
+    string title;
+    string author;
     bool inputrecieved = false;
     
     while(!inputrecieved) //while loop that initiates the user with a menu to select an input and decide what he would like to do with Library.
@@ -20,12 +23,13 @@ int main()
 
         try // A try catch statement inside this while statement to prompt user and ensure that it will loop back around and re execute 
         {
-             cout << "Welcome to the GCC computer science library!!\nBelow you will find a menu that will ask you to decide what to do next.\n" << endl;  
+             cout << "Welcome to the GCC computer science library!!\n" << endl;
+             cout << "Below you will find a menu that will ask you to decide what to do next.\n" << endl;  
              cout << "If checking for book info in library please press 1 to go to book search menu and prompt.\n" << endl;
              cout << "If information is already known and you wish to borrow a book from the library please press 2 to go to the next menu and prompt.\n" << endl;
              cout << "If information is already known and you wish to add a book to the library please press 3 to go to the next menu and prompt.\n" << endl;
              cout << "If information is already known and you wish to remove the book from the library please press 4 to go to the next menu and prompt.\n" << endl;
-             cin >> selection;
+             std::cin >> selection;
              //Created 4 cout statements that each display a different message for the menu and have the user input their selection
              //When the user inputs their selection. If valid, will loop into this if else decision statement chain.
                 if(selection == 1)
@@ -38,6 +42,16 @@ int main()
                 }
                 else if(selection == 3)
                 {
+                    cout << "Thank you for inputting the third selection and enter the add book to archive section of the library.\n" << endl;
+                    cout << "Please enter the name or title of the book you would like to add to the library: " << endl;
+                    cin >> title;
+                    cout << "Please enter the name of the author of the book that is being added to the library: " << endl;
+                    cin >> author;
+                    cout << "Please enter the year in which the book being added was published: " << endl;
+                    cin >> publicationYear;
+                    cout << "Please also declare that amount of copies that you would be adding of said book to the Library: " << endl;
+                    cin >> copiesAvailable;
+                
                     inputrecieved = true;
                 }
                 else if (selection == 4)
@@ -48,13 +62,15 @@ int main()
             if(std::cin.fail())
             {
                 throw std::invalid_argument("Incorrect input has occured. Please read menu selection again and ensure a proper input.");//Like in the factorial calculator previously created it throws an exception
-            
+                
             }
         }
         catch(const exception& e)
         {
             cout << "An error has occured." << endl;
-            std::cin.clear();
+            std::cin.clear(); //clears the error flags
+            std::cin.ignore(10000, '\n'); //discards the invalid input of numbers.
+            
         }
         
 
@@ -68,35 +84,6 @@ void Library::AddBook(std::string title, std::string author, int publicationYear
 {
     //create new unique ptr of book with data from parameters
     //add to book vector
-    cout << "Thank you for inputting the third selection and enter the add book to archive section of the library.\n" << endl;
-    cout << "Please enter the name or title of the book you would like to add to the library: " << endl;
-    cin >> title;
-        if(std::cin.fail())
-        {
-            cout << "Error has occured in input. Please try again by inputing the title or name of the book you wish to add: " << endl;
-            cin >> title;
-        }
-    cout << "Please enter the name of the author of the book that is being added to the library: " << endl;
-    cin >> author;
-        if(std::cin.fail())
-        {
-            cout << "Error has occured in input. Please try again by inputing the name of the author again please: " << endl;
-            cin >> author;
-        }
-    cout << "Please enter the year in which the book being added was published: " << endl;
-    cin >> publicationYear;
-            if(std::cin.fail())
-        {
-            cout << "Error has occured in input. Please try again by inputing the year that in which the book was published: " << endl;
-            cin >> publicationYear;
-        }
-    cout << "Please also declare that amount of copies that you would be adding of said book to the Library: " << endl;
-    cin >> copiesAvailable;
-        if(std::cin.fail())
-        {
-            cout << "Error has occured in input. Please try again by inputing the amount of books you would like to add: " << endl;
-            cin >> copiesAvailable;
-        }
     
     //return AddBook(title,author,publicationYear,copiesAvailable);
 }
