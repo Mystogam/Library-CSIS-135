b07e16033b70ca79ed429f40a7f5716aae8e65c3
Christopher Alexan <chrisalexan123@yahoo.com>
2025-09-02 19:02:01 -0700

    cleaning up what little comments or formatting that appears to be needed in the header file.

--- .vscode/Library.h
+++ .vscode/Library.h
@@ -31,11 +31,9 @@ struct Book //Book struct declaration in header file
 class Library //Delcaring Library Class in header file to be implemented in cpp file
 {
 private:
-    std::vector<std::unique_ptr<Book>> _books; //private vector defined for Books in library 
-    //bookPtr->title
-    //bookPtr->publicationYear
+    std::vector<std::unique_ptr<Book>> _books; //private vector defined for Books in library
 public:
-    void DisplayBook(); //Declaring Display Book function. A function that displays the entire library of books with all their information. Like title, author, publication year and copies they have available. which displays the smart vector.
+    void DisplayBook(); //Declaring Display Book function. A function that displays the entire library of books with all their information. Like title, author, publication year and copies they have available which displays the smart vector
     void BorrowBook(std::string title);//Declaring Borrow Book function that borrows a book from the library adjusting the copiesAvailable and copiesCheckedOut variables in vector.
     void RemoveBook(std::string title);//Declaring the remove book function that removes a book from the function and loops it back to the vector
     void AddBook(std::string title, std::string author, int publicationYear, int copiesAvailable);//Declaring the addbook function that adds a book to the library and has the user introduce all the different values and strings for it.
